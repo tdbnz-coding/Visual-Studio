3a4353ec293917d7cbb0950d13fb61fcbf4c7f35
tdbnz123 <thomasnz@hotmail.com>
2019-04-23 18:19:08 +1200

    Hello World
    *Updated Hello World + Hello World .zip added

---
+++ Hello World/Hello World/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MsgBox("Program Created by Thomas Bernard", MsgBoxStyle.OkOnly, "About")
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem1.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Hello World", MsgBoxStyle.OkOnly, "Hello World")
+    End Sub
+End Class
